31fc79da36e57f45fbf6f431aaa90d562b6e5edd
Nicolas rodriguez <nicolas.rlasso@gmail.com>
2020-12-11 09:22:58 -0500

    bundle up javascrit

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -3,12 +3,12 @@ import { render } from "react-dom";
 
 export default class App extends Component {
     constructor(props){
-        super(props)
+        super(props);
     }
     render (){
-        return <h1>Testing React Code</h1>
+        return <h1>Testing React Code</h1>;
     }
 }
 
-const appDic = document.getElementById("app");
+const appDiv = document.getElementById("app");
 render (<App /> ,appDiv);
