6fed5d5967f438ac0d9766f537c176873e9254c6
Nicolas rodriguez <nicolas.rlasso@gmail.com>
2020-12-22 10:41:14 -0500

    return to home page with leave room

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -77,3 +77,12 @@ class UserInRoom(APIView):
         }
         return JsonResponse(data,status=status.HTTP_200_OK)
         
+class LeaveRoom(APIView):
+    def post(self,request,format=None):
+        if 'room_code' in self.request.session:
+            host_id = self.request.session.session_key
+            room_result = Room.objects.filter(host=host_id)
+            if len(room_result)>0:
+                room=room_result[0]
+                room.delete()
+        return Response({'Message':'Success'},status=status.HTTP_200_OK)
